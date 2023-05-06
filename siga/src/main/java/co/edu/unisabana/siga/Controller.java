914bd08009f3e4511c79fc77e5f5dc3885b66733
SantiagoSantafe <andressasi@unisabana.edu.co>
2023-05-06 17:36:03 -0500

    Done "actualizar" method

--- siga/src/main/java/co/edu/unisabana/siga/Controller.java
+++ siga/src/main/java/co/edu/unisabana/siga/Controller.java
@@ -1,4 +1,5 @@
 package co.edu.unisabana.siga;
+
 import java.util.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,21 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    List<Estudiante> estudianteList=new ArrayList<>();
-    
-    /*public Controller() {
+    List<Estudiante> estudianteList = new ArrayList<>();
+
+    public Controller() {
         this.estudianteList = new ArrayList<>();
         estudianteList.add(new Estudiante("Santiago", 2525, 3, "femenino"));
         estudianteList.add(new Estudiante("xoxo", 2452, 3, "Masculino"));
         estudianteList.add(new Estudiante("caca", 4132, 3, "Masculino"));
         estudianteList.add(new Estudiante("fsagv", 1522, 3, "Masculino"));
         estudianteList.add(new Estudiante("afas", 8994, 4, "Masculino"));
-    }*/
+    }
 
     @GetMapping(path = "/saludameporfavor")
     public String saludar() {
         return "hola";
     }
+
     @GetMapping(path = "/estudiantes/todos")
     public List<Estudiante> listaest() {
         return estudianteList;
@@ -32,28 +34,41 @@ public class Controller {
 
     @GetMapping(path = "/estudiantes")
     public List<Estudiante> obtenerEstporSemestre(@RequestParam int semestre, @RequestParam String facultad) {
-        List<Estudiante> busqueda= new ArrayList<>();
-        for (Estudiante estudiante: estudianteList){
-            if (estudiante.getSemestre()== semestre && estudiante.getFacultad().equals(facultad)) {
+        List<Estudiante> busqueda = new ArrayList<>();
+        for (Estudiante estudiante : estudianteList) {
+            if (estudiante.getSemestre() == semestre && estudiante.getFacultad().equals(facultad)) {
                 busqueda.add(estudiante);
             }
         }
         return busqueda;
     }
-    @GetMapping (path = "/estudiante/{codigo}")
+
+    @GetMapping(path = "/estudiante/{codigo}")
     public Estudiante obtenerEstudianteporCodigo(@PathVariable int codigo) {
-        for (Estudiante estudiante: estudianteList){
-            if (estudiante.getCodigo()== codigo) {
-               return estudiante;
+        for (Estudiante estudiante : estudianteList) {
+            if (estudiante.getCodigo() == codigo) {
+                return estudiante;
             }
         }
         return new Estudiante();
     }
-    @PostMapping (path = "/estudiante/agregar")
+
+    @PostMapping(path = "/estudiante/agregar")
     public String agregarEstudiante(@RequestBody Estudiante estudiante) {
-        estudiante.setCodigo((int)(Math.random()*1000));
+        estudiante.setCodigo((int) (Math.random() * 1000));
         estudianteList.add(estudiante);
         return "Estudiante ingresado correctamente";
     }
-    
+
+    @GetMapping(path = "/estudiante/actualizar/{codigo}")
+    public void actualizarEstudiante(@PathVariable int codigo, @RequestBody Estudiante estudiante) {
+        for (Estudiante estudiante2 : estudianteList) {
+            if (estudiante2.getCodigo()== codigo) {
+                estudiante2.setFacultad(estudiante.getFacultad());
+                estudiante2.setNombre(estudiante.getNombre());
+                estudiante2.setSemestre(estudiante.getSemestre());
+            }
+        }
+
+    }
 }
